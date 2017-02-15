cc9c51bad77fb8c9ee7f54a48a72981831c735d1
Julia Mays <jm27746@gmail.com>
2017-02-15 14:36:01 +0000

    finished yelp

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+
+  resources :restaurants do
+    collection do
+      get "top", to: "restaurants#top"
+    end
+    member do
+      get "chef", to: "restaurants#chef"
+    end
+    resources :reviews, only: [:new, :create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
